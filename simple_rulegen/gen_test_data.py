54ee88244d4b0b052d8a2e3775558c60eb4c2b4f
EmilMachine <emil.lime@gmail.com>
2017-07-10 21:26:23 +0200

    converted json data to strings

--- simple_rulegen/gen_test_data.py
+++ simple_rulegen/gen_test_data.py
@@ -6,6 +6,10 @@ iris = load_iris()
 data_df = pd.DataFrame(data= np.c_[iris['data'], iris['target']],
                      columns= iris['feature_names'] + ['target'])
 
+# convert all to strings
+for col in data_df:
+    data_df[col] = data_df[col].astype(str)
+
 data_df.to_csv('./test_data.csv',index=False)
 data_df.to_json(path_or_buf='./test_data.json',orient='records')
 
